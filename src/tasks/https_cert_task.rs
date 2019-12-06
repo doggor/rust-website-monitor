140e2adfef325362c272a39bf1452129c48e5fe1
Andy Ching <dogdoglization@gmail.com>
2019-12-06 14:41:37 +0800

    fix expiration checking with actived sites only

--- src/tasks/https_cert_task.rs
+++ src/tasks/https_cert_task.rs
@@ -17,19 +17,21 @@ impl Task for HttpsCertTask {
     fn run(&self) -> TaskResult {
         if let Ok(sites) = self.storage.list_sites() {
             for site in sites {
-                match SslExpiration::from_domain_name(&site.domain) {
-                    Ok(expiration) => {
-                        let expiration_secs = expiration.secs();
-                        let dt = Utc::now() + Duration::seconds(expiration_secs.into());
-                        let dt = dt.format("%Y-%m-%d %H:%M:%S").to_string();
-                        let updated = self.storage.update_site(site.id, None, Some(&dt[..]), None);
-                        if let Err(err) = updated {
-                            println!("Fail to update expiry date of site {}: {:?}", site.domain, err);
+                if site.active {
+                    match SslExpiration::from_domain_name(&site.domain) {
+                        Ok(expiration) => {
+                            let expiration_secs = expiration.secs();
+                            let dt = Utc::now() + Duration::seconds(expiration_secs.into());
+                            let dt = dt.format("%Y-%m-%d %H:%M:%S").to_string();
+                            let updated = self.storage.update_site(site.id, None, Some(&dt[..]), None);
+                            if let Err(err) = updated {
+                                println!("Fail to update expiry date of site {}: {:?}", site.domain, err);
+                            }
+                            println!("{} expired at {} {}", site.domain, dt, expiration_secs);
+                        },
+                        Err(err) => {
+                            println!("{:?}", err);
                         }
-                        println!("{} expired at {} {}", site.domain, dt, expiration_secs);
-                    },
-                    Err(err) => {
-                        println!("{:?}", err);
                     }
                 }
             }
